3d4b872f44fee45c9b4f4a1a43739dbe7a509b68
tester214pro <123368658+tester214pro@users.noreply.github.com>
2023-01-26 18:31:42 +0000

    Pending changes exported from your codespace

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+# Создаем экземпляр бота
+bot = telebot.TeleBot('5955466488:AAHgKxmA1OzaDO7N63qAgbRW7TQ4r9Iht9o')
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
